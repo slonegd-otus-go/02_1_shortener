3405ffedcb402e997b7166a47dc4395ee6ce7da5
slonegd <slonkdv@mail.ru>
2019-06-08 10:51:32 +0300

    change example

--- example/main.go
+++ example/main.go
@@ -3,9 +3,14 @@ package main
 import shortener "github.com/slonegd-otus-go/02_1_shortener"
 
 func main() {
-	chars := []rune{'0', '1', '2'}
-	shortener := shortener.New(2, chars)
-	long := "text"
+	chars := []rune{'a', 'b', 'c'}
+	shortener := shortener.New(6, chars)
+
+	long := "otus.ru/long_url"
 	short := shortener.Shorten(long)
-	println(short)
+	println(short, shortener.Resolve(short))
+
+	long = "otus.ua/long_url"
+	short = shortener.Shorten(long)
+	println(short, shortener.Resolve(short))
 }
